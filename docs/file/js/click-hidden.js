359dbb492993571e5ebd0969c34c126758434a6c
nuzulam <141063583+nuzulam@users.noreply.github.com>
2023-08-05 01:01:36 +0700

    Create click-hidden.js

---
+++ docs/file/js/click-hidden.js
@@ -0,0 +1,33 @@
+
+function bukaslot1() {
+				document.getElementById ("slot-on-1").style.display ="block";
+				document.getElementById ("tutupslot1").style.display ="inline";
+				document.getElementById ("bukaslot1").style.display ="none";
+				}
+				function tutupslot1() {
+			  document.getElementById ("slot-off-1").style.display ="none";
+			  document.getElementById ("tutupslot1").style.display ="none";
+			  document.getElementById ("bukaslot1").style.display ="inline";
+				}
+
+function bukaslot2() {
+				document.getElementById ("slot-on-2").style.display ="block";
+				document.getElementById ("tutupslot2").style.display ="inline";
+				document.getElementById ("bukaslot2").style.display ="none";
+				}
+				function tutupslot2() {
+			  document.getElementById ("slot-off-2").style.display ="none";
+			  document.getElementById ("tutupslot2").style.display ="none";
+			  document.getElementById ("bukaslot2").style.display ="inline";
+				}
+  
+function bukaslot3() {
+				document.getElementById ("slot-on-3").style.display ="block";
+				document.getElementById ("tutupslot3").style.display ="inline";
+				document.getElementById ("bukaslot3").style.display ="none";
+				}
+				function tutupslot3() {
+			  document.getElementById ("slot-off-3").style.display ="none";
+			  document.getElementById ("tutupslot3").style.display ="none";
+			  document.getElementById ("bukaslot3").style.display ="inline";
+				}
