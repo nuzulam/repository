5aa5b2c10160806cff2ca3041a1cc92a9f5f2a6e
nuzulam <141063583+nuzulam@users.noreply.github.com>
2023-08-07 03:34:16 +0700

    Update click-hidden.js

--- docs/file/js/click-hidden.js
+++ docs/file/js/click-hidden.js
@@ -1,4 +1,3 @@
-
 function bukaslot1() {
 				document.getElementById ("slot-on-1").style.display ="block";
 				document.getElementById ("tutupslot1").style.display ="inline";
@@ -30,4 +29,125 @@ function bukaslot3() {
 			  document.getElementById ("slot-off-3").style.display ="none";
 			  document.getElementById ("tutupslot3").style.display ="none";
 			  document.getElementById ("bukaslot3").style.display ="inline";
+				}
+  
+function bukaslot4() {
+				document.getElementById ("slot-on-4").style.display ="block";
+				document.getElementById ("tutupslot4").style.display ="inline";
+				document.getElementById ("bukaslot4").style.display ="none";
+				}
+				function tutupslot4() {
+			  document.getElementById ("slot-off-4").style.display ="none";
+			  document.getElementById ("tutupslot4").style.display ="none";
+			  document.getElementById ("bukaslot4").style.display ="inline";
+				}
+  
+function bukaslot5() {
+				document.getElementById ("slot-on-5").style.display ="block";
+				document.getElementById ("tutupslot5").style.display ="inline";
+				document.getElementById ("bukaslot5").style.display ="none";
+				}
+				function tutupslot5() {
+			  document.getElementById ("slot-off-5").style.display ="none";
+			  document.getElementById ("tutupslot5").style.display ="none";
+			  document.getElementById ("bukaslot5").style.display ="inline";
+				}
+  
+function bukaslot6() {
+				document.getElementById ("slot-on-6").style.display ="block";
+				document.getElementById ("tutupslot6").style.display ="inline";
+				document.getElementById ("bukaslot6").style.display ="none";
+				}
+				function tutupslot6() {
+			  document.getElementById ("slot-off-6").style.display ="none";
+			  document.getElementById ("tutupslot6").style.display ="none";
+			  document.getElementById ("bukaslot6").style.display ="inline";
+				}
+  
+function bukaslot7() {
+				document.getElementById ("slot-on-7").style.display ="block";
+				document.getElementById ("tutupslot7").style.display ="inline";
+				document.getElementById ("bukaslot7").style.display ="none";
+				}
+				function tutupslot7() {
+			  document.getElementById ("slot-off-7").style.display ="none";
+			  document.getElementById ("tutupslot7").style.display ="none";
+			  document.getElementById ("bukaslot7").style.display ="inline";
+				}
+  
+function bukaslot8() {
+				document.getElementById ("slot-on-8").style.display ="block";
+				document.getElementById ("tutupslot8").style.display ="inline";
+				document.getElementById ("bukaslot8").style.display ="none";
+				}
+				function tutupslot8() {
+			  document.getElementById ("slot-off-8").style.display ="none";
+			  document.getElementById ("tutupslot8").style.display ="none";
+			  document.getElementById ("bukaslot8").style.display ="inline";
+				}
+  
+function bukaslot9() {
+				document.getElementById ("slot-on-9").style.display ="block";
+				document.getElementById ("tutupslot9").style.display ="inline";
+				document.getElementById ("bukaslot9").style.display ="none";
+				}
+				function tutupslot9() {
+			  document.getElementById ("slot-off-9").style.display ="none";
+			  document.getElementById ("tutupslot9").style.display ="none";
+			  document.getElementById ("bukaslot9").style.display ="inline";
+				}
+  
+function bukaslot10() {
+				document.getElementById ("slot-on-10").style.display ="block";
+				document.getElementById ("tutupslot10").style.display ="inline";
+				document.getElementById ("bukaslot10").style.display ="none";
+				}
+				function tutupslot10() {
+			  document.getElementById ("slot-off-10").style.display ="none";
+			  document.getElementById ("tutupslot10").style.display ="none";
+			  document.getElementById ("bukaslot10").style.display ="inline";
+				}
+  
+function bukaslot11() {
+				document.getElementById ("slot-on-11").style.display ="block";
+				document.getElementById ("tutupslot11").style.display ="inline";
+				document.getElementById ("bukaslot11").style.display ="none";
+				}
+				function tutupslot11() {
+			  document.getElementById ("slot-off-11").style.display ="none";
+			  document.getElementById ("tutupslot11").style.display ="none";
+			  document.getElementById ("bukaslot11").style.display ="inline";
+				}
+  
+function bukaslot12() {
+				document.getElementById ("slot-on-12").style.display ="block";
+				document.getElementById ("tutupslot12").style.display ="inline";
+				document.getElementById ("bukaslot12").style.display ="none";
+				}
+				function tutupslot12() {
+			  document.getElementById ("slot-off-12").style.display ="none";
+			  document.getElementById ("tutupslot12").style.display ="none";
+			  document.getElementById ("bukaslot12").style.display ="inline";
+				}
+  
+function bukaslot13() {
+				document.getElementById ("slot-on-13").style.display ="block";
+				document.getElementById ("tutupslot13").style.display ="inline";
+				document.getElementById ("bukaslot13").style.display ="none";
+				}
+				function tutupslot13() {
+			  document.getElementById ("slot-off-13").style.display ="none";
+			  document.getElementById ("tutupslot13").style.display ="none";
+			  document.getElementById ("bukaslot13").style.display ="inline";
+				}
+
+function bukaslot14() {
+				document.getElementById ("slot-on-14").style.display ="block";
+				document.getElementById ("tutupslot14").style.display ="inline";
+				document.getElementById ("bukaslot14").style.display ="none";
+				}
+				function tutupslot14() {
+			  document.getElementById ("slot-off-14").style.display ="none";
+			  document.getElementById ("tutupslot14").style.display ="none";
+			  document.getElementById ("bukaslot14").style.display ="inline";
 				}
