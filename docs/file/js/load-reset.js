c1ab34c5458134d03dad27792ca0dd77da9f6d65
nuzulam <141063583+nuzulam@users.noreply.github.com>
2023-10-11 03:09:06 +0700

    Create load-reset.js

---
+++ docs/file/js/load-reset.js
@@ -0,0 +1,58 @@
+
+function loadA() {
+            for (let anime in obj) {
+                let elem = document.createElement("option");
+                elem.value = obj[anime].title;
+                
+                document.getElementById("view-select").append(elem);
+            };
+          loadR();
+        };
+ 
+  function loadR() {
+            var elems = document.getElementById("root-router").elements;
+
+
+          const element_001 = document.getElementById("tabsel-001");
+          document.getElementById("tabsel-001").innerHTML= element_001.innerHTML;
+          
+          const element_002 = document.getElementById("tabsel-002");
+          document.getElementById("tabsel-002").innerHTML= element_002.innerHTML;
+          
+          const element_003 = document.getElementById("tabsel-003");
+          document.getElementById("tabsel-003").innerHTML= element_003.innerHTML;
+          
+          const element_004 = document.getElementById("tabsel-004");
+          document.getElementById("tabsel-004").innerHTML= element_004.innerHTML;
+          
+          const element_005 = document.getElementById("tabsel-005");
+          document.getElementById("tabsel-005").innerHTML= element_005.innerHTML;
+          
+          const element_006 = document.getElementById("tabsel-006");
+          document.getElementById("tabsel-006").innerHTML= element_006.innerHTML;
+          
+          const element_007 = document.getElementById("tabsel-007");
+          document.getElementById("tabsel-007").innerHTML= element_007.innerHTML;
+          
+          const element_008 = document.getElementById("tabsel-008");
+          document.getElementById("tabsel-008").innerHTML= element_008.innerHTML;
+
+          const element_009 = document.getElementById("tabsel-009");
+          document.getElementById("tabsel-009").innerHTML= element_009.innerHTML;
+    
+    const element_010 = document.getElementById("tabsel-010");
+    document.getElementById("tabsel-010").innerHTML= element_010.innerHTML;
+
+    const element_011 = document.getElementById("tabsel-011");
+    document.getElementById("tabsel-011").innerHTML= element_011.innerHTML;
+    const element_012 = document.getElementById("tabsel-012");
+    document.getElementById("tabsel-012").innerHTML= element_012.innerHTML;
+    const element_013 = document.getElementById("tabsel-013");
+    document.getElementById("tabsel-013").innerHTML= element_013.innerHTML;
+    
+    
+    
+
+          
+true();
+};
