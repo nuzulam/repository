7f4462da1e8a53e1bfb94ff2dd0325711e0d7672
nuzulam <141063583+nuzulam@users.noreply.github.com>
2023-11-09 01:26:59 +0700

    Update tafsir-digital.js

--- docs/file/js/tafsir-digital.js
+++ docs/file/js/tafsir-digital.js
@@ -65,17 +65,110 @@ on__003.addEventListener("click", ShowTsr003);
 close__003.addEventListener("click", HideTsr003);
 over__003.addEventListener("click", HideTsr003);
   
+// Tafsir View 004
+const on__004 = document.querySelector(".on__004");
+const tsr__004 = document.querySelector(".tsr__004");
+const close__004 = document.querySelector(".close__004 .back_to");
+const over__004 = document.querySelector(".over__004");
+
+function ShowTsr004() {
+  on__004.setAttribute("hidden", "");
+  tsr__004.removeAttribute("hidden");
+  over__004.removeAttribute("hidden");
+};
+
+function HideTsr004() {
+  tsr__004.setAttribute("hidden", "");
+  over__004.setAttribute("hidden", "");
+  on__004.removeAttribute("hidden");
+};
+
+on__004.addEventListener("click", ShowTsr004);
+close__004.addEventListener("click", HideTsr004);
+over__004.addEventListener("click", HideTsr004);
   
+// Tafsir View 005
+const on__005 = document.querySelector(".on__005");
+const tsr__005 = document.querySelector(".tsr__005");
+const close__005 = document.querySelector(".close__005 .back_to");
+const over__005 = document.querySelector(".over__005");
+
+function ShowTsr005() {
+  on__005.setAttribute("hidden", "");
+  tsr__005.removeAttribute("hidden");
+  over__005.removeAttribute("hidden");
+};
+
+function HideTsr005() {
+  tsr__005.setAttribute("hidden", "");
+  over__005.setAttribute("hidden", "");
+  on__005.removeAttribute("hidden");
+};
+
+on__005.addEventListener("click", ShowTsr005);
+close__005.addEventListener("click", HideTsr005);
+over__005.addEventListener("click", HideTsr005);
   
+// Tafsir View 006
+const on__006 = document.querySelector(".on__006");
+const tsr__006 = document.querySelector(".tsr__006");
+const close__006 = document.querySelector(".close__006 .back_to");
+const over__006 = document.querySelector(".over__006");
+
+function ShowTsr006() {
+  on__006.setAttribute("hidden", "");
+  tsr__006.removeAttribute("hidden");
+  over__006.removeAttribute("hidden");
+};
+
+function HideTsr006() {
+  tsr__006.setAttribute("hidden", "");
+  over__006.setAttribute("hidden", "");
+  on__006.removeAttribute("hidden");
+};
+
+on__006.addEventListener("click", ShowTsr006);
+close__006.addEventListener("click", HideTsr006);
+over__006.addEventListener("click", HideTsr006);
+
+// Tafsir View 007
+const on__007 = document.querySelector(".on__007");
+const tsr__007 = document.querySelector(".tsr__007");
+const close__007 = document.querySelector(".close__007 .back_to");
+const over__007 = document.querySelector(".over__007");
+
+function ShowTsr007() {
+  on__007.setAttribute("hidden", "");
+  tsr__007.removeAttribute("hidden");
+  over__007.removeAttribute("hidden");
+};
+
+function HideTsr007() {
+  tsr__007.setAttribute("hidden", "");
+  over__007.setAttribute("hidden", "");
+  on__007.removeAttribute("hidden");
+};
+
+on__007.addEventListener("click", ShowTsr007);
+close__007.addEventListener("click", HideTsr007);
+over__007.addEventListener("click", HideTsr007);
   
-  
-  
+
+
 // Main Input Quran Digital Js
 const AT_001 = document.getElementById("AYT001");
 const AT_002 = document.getElementById("AYT002");
 const AT_003 = document.getElementById("AYT003");
+const AT_004 = document.getElementById("AYT004");
+const AT_005 = document.getElementById("AYT005");
+const AT_006 = document.getElementById("AYT006");
+const AT_007 = document.getElementById("AYT007");
 
 // Main Input Quran Digital Js
 document.getElementById("TSR001").innerHTML= AT_001.innerHTML;
 document.getElementById("TSR002").innerHTML= AT_002.innerHTML;
 document.getElementById("TSR003").innerHTML= AT_003.innerHTML;
+document.getElementById("TSR004").innerHTML= AT_004.innerHTML;
+document.getElementById("TSR005").innerHTML= AT_005.innerHTML;
+document.getElementById("TSR006").innerHTML= AT_006.innerHTML;
+document.getElementById("TSR007").innerHTML= AT_007.innerHTML;
