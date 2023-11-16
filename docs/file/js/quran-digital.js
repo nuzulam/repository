f68f32f05aa898b6d63ff27f73a739e3bc7eab13
nuzulam <141063583+nuzulam@users.noreply.github.com>
2023-11-17 00:21:26 +0700

    Update quran-digital.js

--- docs/file/js/quran-digital.js
+++ docs/file/js/quran-digital.js
@@ -218,28 +218,3 @@ function HideTsr010() {
 on__010.addEventListener("click", ShowTsr010);
 close__010.addEventListener("click", HideTsr010);
 over__010.addEventListener("click", HideTsr010);
-
-
-// Main Input Ayat Quran Digital Js
-const AV_001 = document.getElementById("ayat-001");
-const AV_002 = document.getElementById("ayat-002");
-const AV_003 = document.getElementById("ayat-003");
-const AV_004 = document.getElementById("ayat-004");
-const AV_005 = document.getElementById("ayat-005");
-const AV_006 = document.getElementById("ayat-006");
-const AV_007 = document.getElementById("ayat-007");
-const AV_008 = document.getElementById("ayat-008");
-const AV_009 = document.getElementById("ayat-009");
-const AV_010 = document.getElementById("ayat-010");
-
-// Main Input Ayat Quran Digital Js
-document.getElementById("seayat-001").innerHTML= AV_001.innerHTML;
-document.getElementById("seayat-002").innerHTML= AV_002.innerHTML;
-document.getElementById("seayat-003").innerHTML= AV_003.innerHTML;
-document.getElementById("seayat-004").innerHTML= AV_004.innerHTML;
-document.getElementById("seayat-005").innerHTML= AV_005.innerHTML;
-document.getElementById("seayat-006").innerHTML= AV_006.innerHTML;
-document.getElementById("seayat-007").innerHTML= AV_007.innerHTML;
-document.getElementById("seayat-008").innerHTML= AV_008.innerHTML;
-document.getElementById("seayat-009").innerHTML= AV_009.innerHTML;
-document.getElementById("seayat-010").innerHTML= AV_010.innerHTML;
