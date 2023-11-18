d7130332f7b13feee1cdc3d889c945f5cdb2e167
nuzulam <141063583+nuzulam@users.noreply.github.com>
2023-11-19 06:32:58 +0700

    Create jQuery-Root-Quran.js

---
+++ docs/file/js/jQuery-Root-Quran.js
@@ -0,0 +1,44 @@
+
+// Javascript Save Input Checkbox 
+var formValues = JSON.parse(localStorage.getItem('formValues')) || {};
+var $checkboxes = $("#page_root_quran :checkbox");
+var $button = $("#allcheck_quran button");
+
+function allChecked(){
+  return $checkboxes.length === $checkboxes.filter(":checked").length;
+}
+
+function updateButtonStatus(){
+  $button.text(allChecked()? "Uncheck all" : "Check all");
+}
+
+function handleButtonClick(){
+  $checkboxes.prop("checked", allChecked()? false : true)
+}
+
+function updateStorage(){
+  $checkboxes.each(function(){
+    formValues[this.id] = this.checked;
+  });
+
+  formValues["buttonText"] = $button.text();
+  localStorage.setItem("formValues", JSON.stringify(formValues));
+}
+
+$button.on("click", function() {
+  handleButtonClick();
+  updateButtonStatus();
+  updateStorage();
+});
+
+$checkboxes.on("change", function(){
+  updateButtonStatus();
+  updateStorage();
+});
+
+// On page load
+$.each(formValues, function(key, value) {
+  $("#" + key).prop('checked', value);
+});
+
+$button.text(formValues["buttonText"]);
