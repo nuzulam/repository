01e13edf4fef268453655004a1031479d62e9725
nuzulam <141063583+nuzulam@users.noreply.github.com>
2023-12-03 00:11:12 +0700

    Create 002.js

---
+++ docs/file/js/qs/002.js
@@ -0,0 +1,44 @@
+
+// Main Input Ayat Quran Digital Js
+const AV_001 = document.getElementById("ayat-001");
+const AV_002 = document.getElementById("ayat-002");
+const AV_003 = document.getElementById("ayat-003");
+const AV_004 = document.getElementById("ayat-004");
+const AV_005 = document.getElementById("ayat-005");
+const AV_006 = document.getElementById("ayat-006");
+const AV_007 = document.getElementById("ayat-007");
+const AV_008 = document.getElementById("ayat-008");
+const AV_009 = document.getElementById("ayat-009");
+const AV_010 = document.getElementById("ayat-010");
+const AV_011 = document.getElementById("ayat-011");
+const AV_012 = document.getElementById("ayat-012");
+const AV_013 = document.getElementById("ayat-013");
+const AV_014 = document.getElementById("ayat-014");
+const AV_015 = document.getElementById("ayat-015");
+const AV_016 = document.getElementById("ayat-016");
+const AV_017 = document.getElementById("ayat-017");
+const AV_018 = document.getElementById("ayat-018");
+const AV_019 = document.getElementById("ayat-019");
+const AV_020 = document.getElementById("ayat-020");
+
+// Main Input Ayat Quran Digital Js
+document.getElementById("seayat-001").innerHTML= AV_001.innerHTML;
+document.getElementById("seayat-002").innerHTML= AV_002.innerHTML;
+document.getElementById("seayat-003").innerHTML= AV_003.innerHTML;
+document.getElementById("seayat-004").innerHTML= AV_004.innerHTML;
+document.getElementById("seayat-005").innerHTML= AV_005.innerHTML;
+document.getElementById("seayat-006").innerHTML= AV_006.innerHTML;
+document.getElementById("seayat-007").innerHTML= AV_007.innerHTML;
+document.getElementById("seayat-008").innerHTML= AV_008.innerHTML;
+document.getElementById("seayat-009").innerHTML= AV_009.innerHTML;
+document.getElementById("seayat-010").innerHTML= AV_010.innerHTML;
+document.getElementById("seayat-011").innerHTML= AV_011.innerHTML;
+document.getElementById("seayat-012").innerHTML= AV_012.innerHTML;
+document.getElementById("seayat-013").innerHTML= AV_013.innerHTML;
+document.getElementById("seayat-014").innerHTML= AV_014.innerHTML;
+document.getElementById("seayat-015").innerHTML= AV_015.innerHTML;
+document.getElementById("seayat-016").innerHTML= AV_016.innerHTML;
+document.getElementById("seayat-017").innerHTML= AV_017.innerHTML;
+document.getElementById("seayat-018").innerHTML= AV_018.innerHTML;
+document.getElementById("seayat-019").innerHTML= AV_019.innerHTML;
+document.getElementById("seayat-020").innerHTML= AV_020.innerHTML;
