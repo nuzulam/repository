ab7447a4603a2c6bc290564e280020890f90246c
nuzulam <141063583+nuzulam@users.noreply.github.com>
2023-12-18 20:22:16 +0700

    Create 114.js

---
+++ docs/file/js/qs/114.js
@@ -0,0 +1,17 @@
+
+// Main Input Ayat Quran Digital Js
+const AV_001 = document.getElementById("ayat-001");
+const AV_002 = document.getElementById("ayat-002");
+const AV_003 = document.getElementById("ayat-003");
+const AV_004 = document.getElementById("ayat-004");
+const AV_005 = document.getElementById("ayat-005");
+const AV_006 = document.getElementById("ayat-006");
+
+
+// Main Input Ayat Quran Digital Js
+document.getElementById("seayat-001").innerHTML= AV_001.innerHTML;
+document.getElementById("seayat-002").innerHTML= AV_002.innerHTML;
+document.getElementById("seayat-003").innerHTML= AV_003.innerHTML;
+document.getElementById("seayat-004").innerHTML= AV_004.innerHTML;
+document.getElementById("seayat-005").innerHTML= AV_005.innerHTML;
+document.getElementById("seayat-006").innerHTML= AV_006.innerHTML;
