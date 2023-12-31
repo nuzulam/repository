54ec17c821a7988e04045747ea801d9386fcf609
nuzulam <141063583+nuzulam@users.noreply.github.com>
2023-12-31 12:57:53 +0700

    Update 106.js

--- docs/file/js/qs/106.js
+++ docs/file/js/qs/106.js
@@ -1,1 +1,8 @@
-
+const AV_001 = document.getElementById("ayat-001");
+document.getElementById("seayat-001").innerHTML= AV_001.innerHTML;
+const AV_002 = document.getElementById("ayat-002");
+document.getElementById("seayat-002").innerHTML= AV_002.innerHTML;
+const AV_003 = document.getElementById("ayat-003");
+document.getElementById("seayat-003").innerHTML= AV_003.innerHTML;
+const AV_004 = document.getElementById("ayat-004");
+document.getElementById("seayat-004").innerHTML= AV_004.innerHTML;
