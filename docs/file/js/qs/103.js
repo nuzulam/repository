3fbe60c83fae607c5fed2564ef92ffbcdf927376
nuzulam <141063583+nuzulam@users.noreply.github.com>
2023-12-31 12:55:25 +0700

    Update 103.js

--- docs/file/js/qs/103.js
+++ docs/file/js/qs/103.js
@@ -1,1 +1,6 @@
-
+const AV_001 = document.getElementById("ayat-001");
+document.getElementById("seayat-001").innerHTML= AV_001.innerHTML;
+const AV_002 = document.getElementById("ayat-002");
+document.getElementById("seayat-002").innerHTML= AV_002.innerHTML;
+const AV_003 = document.getElementById("ayat-003");
+document.getElementById("seayat-003").innerHTML= AV_003.innerHTML;
