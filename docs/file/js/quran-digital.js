60cd108a48e1e66e0eaeb79b1e2af5151e7fc3d4
nuzulam <141063583+nuzulam@users.noreply.github.com>
2024-01-04 04:04:12 +0700

    Update quran-digital.js

--- docs/file/js/quran-digital.js
+++ docs/file/js/quran-digital.js
@@ -1,25 +1,4 @@
-setting = {
-    //Model Popup
-    objModalTafsirBtn: ".link-ayat",
-    objModalCloseBtn: ".overlay__tafsir, .close-tafsir",
-    objModalTafsirAttr: "data-tafsir"
-}  
-$(setting.objModalTafsirBtn).bind("click", function () {
-        if ($(this).attr(setting.objModalTafsirAttr)) {
 
-            var strDataTafsirName = $(this).attr(setting.objModalTafsirAttr);
-
-            
-            //Fade In Modal Pop Up
-            $(".overlay__tafsir, #" + strDataTafsirName).fadeIn();
-
-        }
-    });
-
-//On clicking the modal background
-$(setting.objModalCloseBtn).bind("click", function () {
-        $(".Tafsir").fadeOut();
-    });
 
 // Tafsir View 001
 const on__001 = document.querySelector(".on__001");
