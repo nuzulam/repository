7243faa9326619bfe38fe52b1668e91de2face43
nuzulam <141063583+nuzulam@users.noreply.github.com>
2024-01-04 04:04:34 +0700

    Update quran-digital.js

--- docs/file/jQuery/quran-digital.js
+++ docs/file/jQuery/quran-digital.js
@@ -1,1 +1,23 @@
 
+setting = {
+    //Model Popup
+    objModalTafsirBtn: ".link-ayat",
+    objModalCloseBtn: ".overlay__tafsir, .close-tafsir",
+    objModalTafsirAttr: "data-tafsir"
+}  
+$(setting.objModalTafsirBtn).bind("click", function () {
+        if ($(this).attr(setting.objModalTafsirAttr)) {
+
+            var strDataTafsirName = $(this).attr(setting.objModalTafsirAttr);
+
+            
+            //Fade In Modal Pop Up
+            $(".overlay__tafsir, #" + strDataTafsirName).fadeIn();
+
+        }
+    });
+
+//On clicking the modal background
+$(setting.objModalCloseBtn).bind("click", function () {
+        $(".Tafsir").fadeOut();
+    });
