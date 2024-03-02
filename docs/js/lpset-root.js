400bb019efb05c737e0dd3ae496fdc22a0ca2f4d
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2024-03-02 16:38:23 +0700

    Create lpset-root.js

---
+++ docs/js/lpset-root.js
@@ -0,0 +1,15 @@
+
+const LP_001 = document.getElementById("ayat-001");
+document.getElementById("seayat-001").innerHTML= LP_001.innerHTML;
+const LP_002 = document.getElementById("ayat-002");
+document.getElementById("seayat-002").innerHTML= LP_002.innerHTML;
+const LP_003 = document.getElementById("ayat-003");
+document.getElementById("seayat-003").innerHTML= LP_003.innerHTML;
+const LP_004 = document.getElementById("ayat-004");
+document.getElementById("seayat-004").innerHTML= LP_004.innerHTML;
+const LP_005 = document.getElementById("ayat-005");
+document.getElementById("seayat-005").innerHTML= LP_005.innerHTML;
+const LP_006 = document.getElementById("ayat-006");
+document.getElementById("seayat-006").innerHTML= LP_006.innerHTML;
+const LP_007 = document.getElementById("ayat-007");
+document.getElementById("seayat-007").innerHTML= LP_007.innerHTML;
