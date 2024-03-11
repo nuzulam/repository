3aa6eedd2081b43f470b1b4861b998d80d38e9a8
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2024-03-11 10:52:36 +0700

    Update jq-page.js

--- docs/file/jQuery/jq-page.js
+++ docs/file/jQuery/jq-page.js
@@ -35,6 +35,37 @@ $(userview.objUserCloseBtn).bind("click", function () {
 $(document).ready(function(){
 	
 
+function toggle_test() {
+
+
+$(".test-action").on("click", function(e){
+	      $("body").addClass("show-action noscroll");
+      });
+
+  // Close and Reset the Video Modal
+
+	function close_test() {
+        // re-hide the video modal
+        $("body").removeClass("show-action noscroll");
+}
+
+      // if the 'close' button/element, or the overlay are clicked 
+	    $('body').on('click', '.close-test, .test-notif .overlay', function(event) {
+          // call the close and
+          close_test();
+      });
+      // if the ESC key is tapped
+      $('body').keyup(function(e) {
+          // ESC key maps to keycode `27`
+          if (e.keyCode == 27) { 
+            // call the close and reset function
+            close_test();
+          }
+      });
+	}
+	toggle_test();
+
+	
 function toggle_comment() {
 
 
