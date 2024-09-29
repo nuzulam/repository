722a6e5c59d64f88af651bb0d168ca4d631645f5
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2024-09-29 23:21:20 +0700

    Update post.js

--- docs/jQuery/article/post.js
+++ docs/jQuery/article/post.js
@@ -1,12 +1,135 @@
+
+  // ViewArt-#
+ //  Class VA (VA-1/s.d./VA-10)
+//   Heading Title Tag <h2></h2>
 $(function(){
 function disp(div) {var a = [];
 for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
-$("nav.nav li .arttoc-1").html(a.join(""));}
-disp($(".section-article .content-post-view > #arttoc-1 + h2").toArray().reverse());
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-1").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-1 + h2").toArray().reverse());
 });
 $(function(){
 function disp(div) {var a = [];
 for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
-$("nav.nav li .arttoc-2").html(a.join(""));}
-disp($(".section-article .content-post-view > #arttoc-2 + h2").toArray().reverse());
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-2").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-2 + h2").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-3").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-3 + h2").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-4").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-4 + h2").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-5").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-5 + h2").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-6").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-6 + h2").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-7").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-7 + h2").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-8").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-8 + h2").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-9").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-9 + h2").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-10").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-10 + h2").toArray().reverse());
+});
+
+
+  // ViewArt-1-#
+ //  Class VA (VA-1-1/s.d./VA-1-5)
+//   Heading Title Tag <h3></h3>
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-1-1").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-1-1 + h3").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-1-2").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-1-2 + h3").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-1-3").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-1-3 + h3").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-1-4").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-1-4 + h3").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-1-5").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-1-5 + h3").toArray().reverse());
+});
+
+
+
+  // ViewArt-2-#
+ //  Class VA (VA-2-1/s.d./VA-2-5)
+//   Heading Title Tag <h3></h3>
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-2-1").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-2-1 + h3").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-2-2").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-2-2 + h3").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-2-3").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-2-3 + h3").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-2-4").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-2-4 + h3").toArray().reverse());
+});
+$(function(){
+function disp(div) {var a = [];
+for(var i=0;i<div.length;i++) {a.push(div[ i ].innerHTML);}
+$(".section-article .article-content .content-post .navigasi-list-art nav.nav li a.VA-2-5").html(a.join(""));}
+disp($(".section-article .article-content .content-post .content-post-view > #ViewArt-2-5 + h3").toArray().reverse());
 });
